523c767008c2cfe819ccb10e55b13962662fb26c
ysshwetha <ysshwetha20@gmail.com>
2022-02-04 17:48:15 -0800

    move print to a separate function

--- StockPrice.py
+++ StockPrice.py
@@ -6,6 +6,8 @@
 import requests # HTTP Library
 import json
 
+access_token = 'vHdL4dVNO7abKi7kr5WxA9l02hEhSXsq2SJbUkvF'
+
 # Get the input from user
 ticker_inp = input("Please enter the ticker(s) separated by comma : ")
 
@@ -15,36 +17,37 @@ if ticker_inp=='':
 	quit()
 
 # Define a function to get stock details from YahooFinance API, # Website : https://www.yahoofinanceapi.com/
-
 def getStock(inp):
+	inp = inp.replace(" ","")
 	# We use access token generated from Yahoo Finance Website, Free access token provides 100 API requests per day
-	access_token = 'vHdL4dVNO7abKi7kr5WxA9l02hEhSXsq2SJbUkvF'
 	header_value={'accept':'application/json' , 'X-API-KEY':access_token}
 	url_value=('https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols={0}'.format(inp)) # We use the ticker input provided by user
 	apiresponse = requests.get(url=url_value,headers=header_value) # HTTPS call to the website
 	jsonresponse = json.loads(apiresponse.text) # Convert the results to JSON
-	jsonresult = jsonresponse['quoteResponse']['result']
+	jsonresult = jsonresponse['quoteResponse']['result'] # Extract the results
 	return jsonresult
 
-# Call the Function
-stk_results = getStock(ticker_inp)
 
-# Now let us do some validation
-if len(stk_results) == 0:
-	print('could not find any ticker matching {0}'.format(ticker_inp))
-	quit()
+def printResult(stk_results, ticker_inp):
+	# Now let us do some validation
+	if len(stk_results) == 0:
+		print('No Stock details found for Ticker {0}'.format(ticker_inp))
+		quit()
 
-# Split the tickers into individual values to validate each one
-individual_ticker = ticker_inp.split(',')
+	# Split the tickers into individual values to validate each one
+	individual_ticker = ticker_inp.split(',')
 
-for i in (individual_ticker):
-	found = False
-	for j in list(stk_results):		
-		if i.upper() == j['symbol']:
-			print("The latest stock value for {0}({1}) is : {2:.2f}".format(j['longName'],j['symbol'],j['regularMarketPrice']))
-			found = True
-			break
-		
-	if found == False:
-		print('Ticker ' + i + ' Not found')	
-		
+	for i in (individual_ticker):
+		found = False
+		for j in list(stk_results):		
+			if i.upper().strip() == j['symbol']:
+				print("The latest stock value for {0}({1}) is : {2:.2f}".format(j['longName'],j['symbol'],j['regularMarketPrice']))
+				found = True
+				break
+			
+		if found == False:
+			print('No Stock details found for Ticker ' + i )	
+
+# Call the Stock Function
+stk_results=getStock(ticker_inp)
+printResult(stk_results, ticker_inp)
